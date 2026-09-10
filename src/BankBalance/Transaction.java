ed3920ac612a141f79b7309e631d95529d31fbd2
Matheus Pimentel <mtspimentel@icloud.com>
2026-09-10 11:21:16 -0300

    Adição da classe Transaction.java;

---
+++ src/BankBalance/Transaction.java
@@ -0,0 +1,15 @@
+package BankBalance;
+
+public class Transaction {
+    private String type;
+    private double amount;
+
+    public Transaction(String type, double amount) {
+        this.type = type;
+        this.amount = amount;
+    }
+
+    public String toString() {
+        return type + " - $" + amount;
+    }
+}
